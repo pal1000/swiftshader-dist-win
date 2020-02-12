2f0f48eb411b8a4a6f7bee7e4edf5434fe3458e3
pal1000 <liviuprodea@yahoo.com>
2020-02-12 14:07:07 +0200

    V2: Add components to bin folder that cmake install target misses. Initial attempt didn't work for 32-bit build.

--- buildscript/modules/dist.cmd
+++ buildscript/modules/dist.cmd
@@ -9,11 +9,9 @@
 @forfiles /p %devroot%\%projectname:~0,-9%\build\buildsys-%abi% /s /m *.dll /c "cmd /c copy @path %devroot%\%projectname%\dist\%abi%\bin"
 @forfiles /p %devroot%\%projectname:~0,-9%\build\buildsys-%abi% /s /m *.json /c "cmd /c copy @path %devroot%\%projectname%\dist\%abi%\bin"
 @cd /d %devroot%\%projectname%\dist\%abi%\bin
-@IF NOT EXIST translator md translator
-@move lib*.dll translator
-@move translator\libEGL.dll .
-@move translator\libGLESv2.dll .
-@move translator\libGLES*.dll .
+@IF EXIST translator RD /S /Q translator
+@md translator
+@move *translator.dll translator
 @echo.
 
 :exit
