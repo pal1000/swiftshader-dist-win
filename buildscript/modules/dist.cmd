5c3f706efe33bf7dded5ef6da47999220042487e
pal1000 <liviuprodea@yahoo.com>
2020-02-12 18:50:50 +0200

    More fixes for when SPIRV-Tools install target is missing.

--- buildscript/modules/dist.cmd
+++ buildscript/modules/dist.cmd
@@ -6,6 +6,9 @@
 @if /I NOT "%dist%"=="y" GOTO exit
 @cd %devroot%
 
+@IF NOT EXIST %devroot%\%projectname%\dist md %devroot%\%projectname%\dist
+@IF NOT EXIST %devroot%\%projectname%\dist\%abi% md %devroot%\%projectname%\dist\%abi%
+@IF NOT EXIST %devroot%\%projectname%\dist\%abi%\bin md %devroot%\%projectname%\dist\%abi%\bin
 @forfiles /p %devroot%\%projectname:~0,-9%\build\buildsys-%abi% /s /m *.dll /c "cmd /c copy @path %devroot%\%projectname%\dist\%abi%\bin"
 @forfiles /p %devroot%\%projectname:~0,-9%\build\buildsys-%abi% /s /m *.json /c "cmd /c copy @path %devroot%\%projectname%\dist\%abi%\bin"
 @cd /d %devroot%\%projectname%\dist\%abi%\bin
