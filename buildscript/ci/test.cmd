dce1c92b0429f89b6825a26b626c22e748bfec08
pal1000 <liviuprodea@yahoo.com>
2020-02-21 23:25:17 +0200

    Test Appveyor build title change

---
+++ buildscript/ci/test.cmd
@@ -0,0 +1,6 @@
+@git clone -q --branch=master https://github.com/google/swiftshader C:\projects\swiftshader
+@cd C:\projects\swiftshader
+@FOR /F tokens^=^*^ USEBACKQ^ eol^= %%a IN (`git log -1 --format=format:"%aD"`) DO @set commitdate=%%a
+@FOR /F tokens^=^*^ USEBACKQ^ eol^= %%a IN (`git log -1 --format=format:"%s"`) DO @set commitmessage=%%a
+@FOR /F tokens^=^*^ USEBACKQ^ eol^= %%a IN (`git log -1 --format=format:"%H"`) DO @set commithash=%%a
+@appveyor UpdateBuild -Message "%commitmessage%" -CommitId "%commithash%" -Committed "%commitdate%"
