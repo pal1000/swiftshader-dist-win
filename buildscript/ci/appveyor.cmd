ed0c7add80130d6d176871db7075c5d12ec1da9d
pal1000 <liviuprodea@yahoo.com>
2020-02-22 18:05:15 +0200

    Appveyor CI: Stub remaining build matrix parts

--- buildscript/ci/appveyor.cmd
+++ buildscript/ci/appveyor.cmd
@@ -7,16 +7,25 @@
 @IF /I %cijob:~0,1%==x GOTO run-build
 
 :appveyor-init
-@rem Initial code checkout
-@IF EXIST %devroot%\%projectname% RD /S /Q %devroot%\%projectname%
-@IF EXIST %devroot%\%projectname:~0,-9% RD /S /Q %devroot%\%projectname:~0,-9%
+@rem Clean leftover binaries and build info data
+@IF EXIST %devroot%\%projectname%\dist\buildinfo RD /S /Q %devroot%\%projectname%\dist\buildinfo
+@IF EXIST %devroot%\%projectname%\dist\x??\ RD /S /Q %devroot%\%projectname%\dist\x??
+@rem Get swiftshader source code
 @git clone -q -n https://github.com/google/%projectname:~0,-9% %devroot%\%projectname:~0,-9%
-@git clone -q --branch=%1 https://github.com/pal1000/%projectname% %devroot%\%projectname%
-
+@rem Get / update build script source
+@IF NOT EXIST %devroot%\%projectname% (
+@set firstbuild=1
+@git clone -q --branch=master https://github.com/%APPVEYOR_ACCOUNT_NAME%/%projectname% %devroot%\%projectname%
+)
+@cd %devroot%\%projectname%
+@IF NOT defined firstbuild (
+@git checkout master
+@git pull
+)
+@git checkout %1
 @rem Configure build title
 @cd %devroot%\%projectname:~0,-9%
 @type %devroot%\%projectname%\buildscript\ci\appveyor.ps1 | powershell -NoLogo >nul 2>&1
-
 @rem Save swiftshader commit ID, build start timestamp and its converted form into a version string
 @FOR /F "tokens=* USEBACKQ" %%a IN (`powershell -Command "Get-Date -Format FileDateTimeUniversal"`) do @set artifactuid=%%a
 @set artifactver=%artifactuid%
@@ -24,12 +33,20 @@
 @set artifactver=%artifactver:~0,8%%artifactver:~9,4%
 @IF NOT EXIST %devroot%\%projectname%\dist md %devroot%\%projectname%\dist
 @IF NOT EXIST %devroot%\%projectname%\dist\modules md %devroot%\%projectname%\dist\modules
-@FOR /F "tokens=*" %%a IN ('type %devroot%\%projectname:~0,-9%\.git\refs\heads\master') do @echo @set swiftshadercommit=%%a>%devroot%\%projectname%\dist\modules\uid.cmd
-@echo @set artifactuid=%artifactuid%>>%devroot%\%projectname%\dist\modules\uid.cmd
+@echo @set artifactuid=%artifactuid%>%devroot%\%projectname%\dist\modules\uid.cmd
 @echo @set artifactver=%artifactver%>>%devroot%\%projectname%\dist\modules\uid.cmd
-
+@FOR /F "tokens=*" %%a IN ('type %devroot%\%projectname:~0,-9%\.git\refs\heads\master') do @echo @set swiftshadercommit=%%a>>%devroot%\%projectname%\dist\modules\uid.cmd
 @GOTO doneappveyor
 
+:run-build
+@type %devroot%\%projectname%\dist\modules\uid.cmd
+@GOTO doneappveyor
+
+:artifact-llvm
+@GOTO doneappveyor
+
+:artifact-subzero
+@GOTO doneappveyor
 
 c:\projects\swiftshader-dist-win\buildscript\ci\init.cmd
 @IF NOT %cijob%==init IF NOT %cijob%==artifact-LLVM IF NOT %cijob%==artifact-subzero c:\projects\swiftshader-dist-win\buildscript\build.cmd %cijob%
