b995bac6cab9f1baf6f95634bcf20b0026914853
pal1000 <liviuprodea@yahoo.com>
2020-03-01 18:14:26 +0200

    Try a different approach to get skip build support working. Write code sources unique identifiers to a HTML file

--- buildscript/ci/ci.cmd
+++ buildscript/ci/ci.cmd
@@ -3,29 +3,26 @@
 @cd ..\..\..\
 @for %%a in ("%cd%") do @set devroot=%%~sa
 @set projectname=swiftshader-dist-win
+@IF /I %1==collectuids GOTO collectuids
+@IF /I NOT %1==collectuids GOTO runci
 
-@rem Determine if new build is needed
-@IF EXIST %devroot%\%projectname%\buildscript\ci\assets REN %devroot%\%projectname%\buildscript\ci\assets assets-old
-@md %devroot%\%projectname%\buildscript\ci\assets
+:collectuids
+@rem Collect build sources unique identifiers and push them to CI YAML configuration
 @cd %devroot%\%projectname:~0,-9%
-@FOR /F "tokens=*" %%a IN ('git rev-parse HEAD') do @echo swiftshader-source-code=%%a>%devroot%\%projectname%\buildscript\ci\assets\hashes.ini
+@FOR /F "tokens=*" %%a IN ('git rev-parse HEAD') do @set srcswiftshader=%%a
 @cd %devroot%\%projectname%
-@FOR /F "tokens=*" %%a IN ('git rev-parse HEAD') do @echo swiftshader-dist-win=%%a>>%devroot%\%projectname%\buildscript\ci\assets\hashes.ini
-@echo ---------------------------------------------------------
-@echo Diagnostic data to check if skip build support is viable.
-@echo ---------------------------------------------------------
-@echo Current build UIDs
-@echo ------------------
-@type %devroot%\%projectname%\buildscript\ci\assets\hashes.ini
-@echo -------------------
-@echo Previous build UIDs
-@echo -------------------
-@IF EXIST %devroot%\%projectname%\buildscript\ci\assets-old\hashes.ini type %devroot%\%projectname%\buildscript\ci\assets-old\hashes.ini
-@IF NOT EXIST %devroot%\%projectname%\buildscript\ci\assets-old\hashes.ini echo File not found - %devroot%\%projectname%\buildscript\ci\assets-old\hashes.ini. No previous build data available. Skip build support is disabled.
-@FC /B %devroot%\%projectname%\buildscript\ci\assets-old\hashes.ini %devroot%\%projectname%\buildscript\ci\assets\hashes.ini>NUL 2>&1 &&GOTO skipci
-@cd %devroot%
+@FOR /F "tokens=*" %%a IN ('git rev-parse HEAD') do @set distswiftshader=%%a
+@call %devroot%\%projectname%\buildscript\ci\pushvar.cmd srcswiftshader
+@call %devroot%\%projectname%\buildscript\ci\pushvar.cmd distswiftshader
+@GOTO doneci
 
-@rem Generating version UID and artifact timestamp
+:runci
+@rem Check if build is necessary first
+@IF NOT EXIST %devroot%\%projectname%\buildscript\ci\assets md %devroot%\%projectname%\buildscript\ci\assets
+@echo 1>%devroot%\%projectname%\buildscript\ci\assets\gotuids.ini
+@IF EXIST %devroot%\%projectname%\buildscript\ci\assets\gotuids.ini IF EXIST %devroot%\%projectname%\buildscript\ci\assets-old\gotuids.ini GOTO skipci
+
+@rem Generating version UID and artifact timestamp, push artifact timestamp to CI YAML configuration
 @IF NOT EXIST %devroot%\%projectname%\dist md %devroot%\%projectname%\dist
 @IF NOT EXIST %devroot%\%projectname%\dist\modules md %devroot%\%projectname%\dist\modules
 @FOR /F "tokens=* USEBACKQ" %%a IN (`powershell -Command "Get-Date -Format FileDateTimeUniversal"`) do @set artifactuid=%%a
@@ -34,6 +31,32 @@
 @set artifactver=%artifactver:~0,8%%artifactver:~9,4%
 @echo @set artifactver=%artifactver%>%devroot%\%projectname%\dist\modules\config.cmd
 @echo @set reactorbackend=%1>>%devroot%\%projectname%\dist\modules\config.cmd
+@call %devroot%\%projectname%\buildscript\ci\pushvar.cmd artifactuid
+
+@rem Write build unique identifiers to a HTML document with links to those commits
+@IF NOT EXIST %devroot%\%projectname%\dist\buildinfo md %devroot%\%projectname%\dist\buildinfo
+@echo ^<html^>>%devroot%\%projectname%\dist\buildinfo\sources-unique-identifiers.html
+@echo ^<head^>>%devroot%\%projectname%\dist\buildinfo\sources-unique-identifiers.html
+@echo ^<title^>Sources unique identifiers^</title^>>%devroot%\%projectname%\dist\buildinfo\sources-unique-identifiers.html
+@echo ^</head^>>%devroot%\%projectname%\dist\buildinfo\sources-unique-identifiers.html
+@echo ^<body^>>%devroot%\%projectname%\dist\buildinfo\sources-unique-identifiers.html
+@echo Swiftshaader was built using the folowing code sources^<br^>>%devroot%\%projectname%\dist\buildinfo\sources-unique-identifiers.html
+@echo ^<table^>>%devroot%\%projectname%\dist\buildinfo\sources-unique-identifiers.html
+@echo ^<tr^>>%devroot%\%projectname%\dist\buildinfo\sources-unique-identifiers.html
+@echo ^<th^>Description^</th^>>%devroot%\%projectname%\dist\buildinfo\sources-unique-identifiers.html
+@echo ^<th^>Contents^</th^>>%devroot%\%projectname%\dist\buildinfo\sources-unique-identifiers.html
+@echo ^</tr^>>%devroot%\%projectname%\dist\buildinfo\sources-unique-identifiers.html
+@echo ^<tr^>>%devroot%\%projectname%\dist\buildinfo\sources-unique-identifiers.html
+@echo ^<td^>Google swiftshader source code^</td^>>%devroot%\%projectname%\dist\buildinfo\sources-unique-identifiers.html
+@echo ^<td^>^<a href='https://github.com/google/swiftshader/tree/%srcswiftshader%'^>%srcswiftshader%^</a^>^</td^>>%devroot%\%projectname%\dist\buildinfo\sources-unique-identifiers.html
+@echo ^</tr^>>%devroot%\%projectname%\dist\buildinfo\sources-unique-identifiers.html
+@echo ^<tr^>>%devroot%\%projectname%\dist\buildinfo\sources-unique-identifiers.html
+@echo ^<td^>Build, continous integration and usage utility source code^</td^>>%devroot%\%projectname%\dist\buildinfo\sources-unique-identifiers.html
+@echo ^<td^>^<a href='https://github.com/pal1000/swiftshader-dist-win/tree/%distswiftshader%'^>%distswiftshader%^</a^>^</td^>>%devroot%\%projectname%\dist\buildinfo\sources-unique-identifiers.html
+@echo ^</tr^>>%devroot%\%projectname%\dist\buildinfo\sources-unique-identifiers.html
+@echo ^</table^>>%devroot%\%projectname%\dist\buildinfo\sources-unique-identifiers.html
+@echo ^</body^>>%devroot%\%projectname%\dist\buildinfo\sources-unique-identifiers.html
+@echo ^</html^>>%devroot%\%projectname%\dist\buildinfo\sources-unique-identifiers.html
 
 @rem Run build
 @call %devroot%\%projectname%\buildscript\build.cmd x64-%1
@@ -41,7 +64,11 @@
 @IF EXIST x64\bin\vk_swiftshader_icd.json IF EXIST x64\bin\libEGL.dll IF EXIST x64\bin\vk_swiftshader.dll call %devroot%\%projectname%\buildscript\build.cmd x86-%1
 @cd %devroot%\%projectname%\dist
 @IF EXIST x64\bin\vk_swiftshader_icd.json IF EXIST x64\bin\libEGL.dll IF EXIST x64\bin\vk_swiftshader.dll IF EXIST x86\bin\vk_swiftshader_icd.json IF EXIST x86\bin\libEGL.dll IF EXIST x86\bin\vk_swiftshader.dll 7z a -t7z -mx=9 ..\swiftshader-%artifactuid%-%1.7z .\*
-@call %devroot%\%projectname%\buildscript\ci\pushvar.cmd artifactuid
+@IF NOT EXIST %devroot%\%projectname%\swiftshader-%artifactuid%-%1.7z GOTO doneci
+
+@rem Prepare cache if we have build artifact
+@IF EXIST %devroot%\%projectname%\buildscript\ci\assets-old RD /S /Q %devroot%\%projectname%\buildscript\ci\assets-old
+@IF EXIST %devroot%\%projectname%\buildscript\ci\assets REN %devroot%\%projectname%\buildscript\ci\assets assets-old
 @GOTO doneci
 
 :skipci
--- buildscript/modules/envdump.cmd
+++ buildscript/modules/envdump.cmd
@@ -7,15 +7,15 @@
 @IF /I NOT "%enableenvdump%"=="y" GOTO skipenvdump
 @echo Dumping build environment information. This will take a short while...
 @echo.
-@echo Build environment>%devroot%\%projectname%\dist\buildinfo\msvc.txt
-@echo ----------------->>%devroot%\%projectname%\dist\buildinfo\msvc.txt
+@echo Build environment>%devroot%\%projectname%\dist\buildinfo\environment.txt
+@echo ----------------->>%devroot%\%projectname%\dist\buildinfo\environment.txt
 
 @rem Dump Windows version
 @FOR /F "USEBACKQ tokens=2 delims=[" %%a IN (`ver`) DO @set winver=%%a
 @set winver=%winver:~0,-1%
 @FOR /F "USEBACKQ tokens=2 delims= " %%a IN (`echo %winver%`) DO @set winver=%%a
 @FOR /F "USEBACKQ tokens=1-3 delims=." %%a IN (`echo %winver%`) DO @set winver=%%a.%%b.%%c
-@echo Windows %winver%>>%devroot%\%projectname%\dist\buildinfo\msvc.txt
+@echo Windows %winver%>>%devroot%\%projectname%\dist\buildinfo\environment.txt
 
 @rem Dump 7-Zip version and compression level
 @set sevenzpath=1
@@ -28,37 +28,37 @@ set "exitloop="
 SET sevenzipver=%%a
 )
 @IF NOT defined sevenzipver IF EXIST %devroot%\%projectname%\buildscript\assets\sevenzip.txt set /p sevenzipver=<%devroot%\%projectname%\buildscript\assets\sevenzip.txt
-@IF defined sevenzipver echo 7-Zip %sevenzipver%>>%devroot%\%projectname%\dist\buildinfo\msvc.txt
+@IF defined sevenzipver echo 7-Zip %sevenzipver%>>%devroot%\%projectname%\dist\buildinfo\environment.txt
 
 @rem Get Git version
 @IF NOT %gitstate%==0 FOR /F "USEBACKQ tokens=3" %%a IN (`git --version`) do @set gitver=%%a
 @IF NOT %gitstate%==0 set "gitver=%gitver:.windows=%"
-@IF defined gitver echo Git %gitver%>>%devroot%\%projectname%\dist\buildinfo\msvc.txt
+@IF defined gitver echo Git %gitver%>>%devroot%\%projectname%\dist\buildinfo\environment.txt
 
 @rem Dump Visual Studio environment
-@echo %msvcname% v%msvcver%>>%devroot%\%projectname%\dist\buildinfo\msvc.txt
+@echo %msvcname% v%msvcver%>>%devroot%\%projectname%\dist\buildinfo\environment.txt
 @call %vsenv% %vsabi%>nul 2>&1
-@echo Windows SDK %WindowsSDKVersion:~0,-1%>>%devroot%\%projectname%\dist\buildinfo\msvc.txt
+@echo Windows SDK %WindowsSDKVersion:~0,-1%>>%devroot%\%projectname%\dist\buildinfo\environment.txt
 
 @rem Dump Python environment
-@echo Python %pythonver%>>%devroot%\%projectname%\dist\buildinfo\msvc.txt
-@echo.>>%devroot%\%projectname%\dist\buildinfo\msvc.txt
-@echo Python packages>>%devroot%\%projectname%\dist\buildinfo\msvc.txt
-@echo --------------->>%devroot%\%projectname%\dist\buildinfo\msvc.txt
-@FOR /F "USEBACKQ skip=2 tokens=*" %%a IN (`%pythonloc% -W ignore -m pip list --disable-pip-version-check`) do @echo %%a>>%devroot%\%projectname%\dist\buildinfo\msvc.txt
-@echo.>>%devroot%\%projectname%\dist\buildinfo\msvc.txt
+@echo Python %pythonver%>>%devroot%\%projectname%\dist\buildinfo\environment.txt
+@echo.>>%devroot%\%projectname%\dist\buildinfo\environment.txt
+@echo Python packages>>%devroot%\%projectname%\dist\buildinfo\environment.txt
+@echo --------------->>%devroot%\%projectname%\dist\buildinfo\environment.txt
+@FOR /F "USEBACKQ skip=2 tokens=*" %%a IN (`%pythonloc% -W ignore -m pip list --disable-pip-version-check`) do @echo %%a>>%devroot%\%projectname%\dist\buildinfo\environment.txt
+@echo.>>%devroot%\%projectname%\dist\buildinfo\environment.txt
 
 @rem Get CMake version
 @IF "%cmakestate%"=="1" set PATH=%devroot%\cmake\bin\;%PATH%
-@IF NOT "%cmakestate%"=="0" IF NOT "%cmakestate%"=="" set exitloop=1&for /f "tokens=3 USEBACKQ" %%a IN (`cmake --version`) do @if defined exitloop set "exitloop="&echo CMake %%a>>%devroot%\%projectname%\dist\buildinfo\msvc.txt
+@IF NOT "%cmakestate%"=="0" IF NOT "%cmakestate%"=="" set exitloop=1&for /f "tokens=3 USEBACKQ" %%a IN (`cmake --version`) do @if defined exitloop set "exitloop="&echo CMake %%a>>%devroot%\%projectname%\dist\buildinfo\environment.txt
 
 @rem Get Ninja version
 @IF "%ninjastate%"=="1" set PATH=%devroot%\ninja\;%PATH%
-@IF NOT "%ninjastate%"=="0" IF NOT "%ninjastate%"=="" for /f "USEBACKQ" %%a IN (`ninja --version`) do @echo Ninja %%a>>%devroot%\%projectname%\dist\buildinfo\msvc.txt
+@IF NOT "%ninjastate%"=="0" IF NOT "%ninjastate%"=="" for /f "USEBACKQ" %%a IN (`ninja --version`) do @echo Ninja %%a>>%devroot%\%projectname%\dist\buildinfo\environment.txt
 
 @rem Finished environment information dump.
 @echo Done.
-@echo Environment information has been written to %devroot%\%projectname%\dist\buildinfo\msvc.txt.
+@echo Environment information has been written to %devroot%\%projectname%\dist\buildinfo\environment.txt.
 @echo.
 
 :skipenvdump
