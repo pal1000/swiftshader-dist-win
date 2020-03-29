4362c75fec71ced706ce915e0e5897e7d562e3ce
pal1000 <liviuprodea@yahoo.com>
2020-03-29 13:09:11 +0300

    Remove an unused build script module

--- buildscript/modules/applypatch.cmd
+++
@@ -1,6 +0,0 @@
-@setlocal
-@echo Applying patch %*...
-@IF %toolchain%==msvc git apply --check --apply %devroot%\mesa-dist-win\patches\%*.patch
-@IF %toolchain%==gcc %msysloc%\usr\bin\bash --login -c "cd ${devroot}/mesa;patch -Np1 --no-backup-if-mismatch -r - -i ${devroot}/mesa-dist-win/patches/%*.patch"
-@echo.
-@endlocal
